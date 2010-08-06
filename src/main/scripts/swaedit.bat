863f6e6dba2b7ed573eeff5e35f55005ef6e1d48
troggie <troggie@39b7773f-8049-0410-a22f-b864f965c3b3>
2010-08-06 18:15:28 +0000

    Trog: fix for library, paths and launching problem for Windows
    git-svn-id: svn://svn.code.sf.net/p/swaedit/code@23 39b7773f-8049-0410-a22f-b864f965c3b3

--- src/main/scripts/swaedit.bat
+++ src/main/scripts/swaedit.bat
@@ -1,5 +1,5 @@
 REM full path to your javaw 1.6+ binary
 set JAVA_EXE=javaw
 
-start /B %JAVA_EXE% -Djava.library.path=lib -cp lib/qtjambi.jar:bin pl.swmud.ns.swaedit.gui.SWAEdit
+start /B %JAVA_EXE% -Djava.library.path=lib -cp lib/qtjambi.jar;bin pl.swmud.ns.swaedit.gui.SWAEdit
 
